da123f585b7492b9ccd34c3ec2f6608baa37ba36
Wanderson Alves <Wanderson.Alves.n@outlook.com>
2022-10-14 00:44:50 -0300

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+export function App() {
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
+        Enviar
+      </button>
+    </div>
+  )
+}
+
+export default App
